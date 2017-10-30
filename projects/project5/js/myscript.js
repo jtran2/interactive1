e3e0f74f04481d1dae241d8ca9058b8b617b2dfb
jtran2 <jtran2@cca.edu>
2017-10-29 17:59:09 -0700

    update

--- projects/project5/js/myscript.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-        var person = prompt("Who are you?", "Harry Potter");
-        
-        if (person != null) {
-            document.getElementById("demo").innerHTML =
-            "Sup " + person + ". Let's go.";
-        }
-    }
